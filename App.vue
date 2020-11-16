8e51b9e244426e03514de2314d226a65fe6de59b
zhangyang <690196784@qq.com>
2020-11-16 18:21:05 +0800

    第一版推送

---
+++ App.vue
@@ -0,0 +1,39 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		}, 
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+    @import "colorui/main.css";
+    @import "colorui/icon.css";
+    @import "./common/uni.css";
+	
+	@font-face {
+		font-family: yticon;
+		font-weight: normal;
+		font-style: normal;
+		src: url('https://at.alicdn.com/t/font_1078604_w4kpxh0rafi.ttf') format('truetype');
+	}
+	
+	.icon-zuojiantou-up:before {
+		content: "\e605";
+	} 
+	
+ 	.yticon {
+		font-family: "yticon" !important;
+		font-size: 16px;
+		font-style: normal;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+</style>
